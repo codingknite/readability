cf80629ad7c8601b1f459a6d1b58accc8d4f0909
andreespirela <60560109+andreespirela@users.noreply.github.com>
2020-09-01 19:16:10 -0400

    Update app.js
    Proposals

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 init();
 
 //All Variables
+// Don't declare your variables outside the scopes they will be needed. Variables must be declared under the scope they are used 
+// so they can be available for garbage-collection and it's better in terms of security
+// use `let` for variables you will constantly re-define and `const` for variables you will only define once.
+// Const must be defined at the time of usage. ex: `you cannot do : const generateButton;` you must assign a value when defining it
 let generateButton, userInput, arrayOfWords, words, letters, sentences, characters, paragraphs, readability, writeBtn, editBtn;
 
 generateButton = document.querySelector('.submit-btn');
@@ -9,39 +13,58 @@ generateButton = document.querySelector('.submit-btn');
 //EVENT LISTENER FOR GENERATE WORD COUNT
 generateButton.addEventListener('click', function () {
     //1. Get Input Value;
+    // Use const here: `userInput`
     userInput = document.getElementById('text-area').value;
 
     //2. Obtain Array of Words
+    // use const here too: `arrayOfWords`
     arrayOfWords = userInput.match(/[\S ]/gi).join('').split(' ');
 
     //3. Generate number of Words, Letters, Sentences, Characters, Paragraphs
 
     //Words
+    // Change this for something more meaningful , like `wordsLength` and use const too.
     words = arrayOfWords.length;
 
     //Letters
+    // More meaningful too `lettersLength` & use const as well
     letters = userInput.match(/[a-z]/gi).length;
 
     //Sentences (. ! ?)
+    // 
     if (userInput.match(/[.!?]/g).length === null) {
+        // More meaning full 
         sentences = 0;
     } else {
+        // More meaning full
         sentences = userInput.match(/[.!?]/g).length;
     };
     //Characters
+    // More meaning full & use const as well
     characters = userInput.match(/\S/g).length;
 
     //Paragraphs
+    // Don't use `=== null` in this case, use  `!` like
+    // if(!userInput.match(/\n/g))
+    // `!` means if not defined or if null in this case.
     if (userInput.match(/\n/g) === null) {
+        // More meaning full 
         paragraphs = 0;
     } else {
+        // More meaning full
         paragraphs = userInput.match(/\n/g).length;
     };
 
     //Calculate Readability
+    // Use const as well here
     readability = Math.floor((0.0588 * letters) - (0.296 * sentences) - 15.8);
 
     //Display To The UI
+    // extract these selectors into an object.
+    // like
+    // const objSelectors = { wordCount: ".word-count", letterCount: ".letter-count" };
+    // then get them like : document.querySelector(objSelectors[wordCount])
+    // this const object will have to go in the global scope (outside any function) because I see you call it from multiple functions...
     document.querySelector('.word-count').textContent = words;
     document.querySelector('.letter-count').textContent = letters;
     document.querySelector('.sentence-count').textContent = sentences;
@@ -49,6 +72,7 @@ generateButton.addEventListener('click', function () {
     document.querySelector('.par-count').textContent = paragraphs;
 
     //Readability
+    // Too much repeated selectors for grade & outlook, extract them as well and put them into the `objSelectors` object I previously recommended
     if (readability > 14) {
         document.querySelector('.grade').textContent = 'Post-Graduate';
         document.querySelector('.grade').style.color = 'red';
@@ -69,6 +93,7 @@ generateButton.addEventListener('click', function () {
 });
 
 //Write Button
+// use const here too
 writeBtn = document.querySelector('.write-btn');
 
 writeBtn.addEventListener('click', function () {
@@ -84,6 +109,7 @@ writeBtn.addEventListener('click', function () {
 });
 
 //Edit Button
+// use const here too
 editBtn = document.querySelector('.edit-btn');
 
 editBtn.addEventListener('click', function () {
@@ -98,6 +124,7 @@ editBtn.addEventListener('click', function () {
     moveCaretToEnd(document.getElementById('text-area'));
 })
 
+// change `el` to `elementEvent` : el is too vague, unreadable
 function moveCaretToEnd(el) {
     el.focus();
     if (typeof el.selectionStart == "number") {
@@ -110,6 +137,7 @@ function moveCaretToEnd(el) {
 }
 
 function init() {
+    // Use the selectors you put in the global object you created previously with my recommendation, the `objSelectors` object.
     document.querySelector('.word-count').textContent = 6;
     document.querySelector('.letter-count').textContent = 22;
     document.querySelector('.sentence-count').textContent = 1;
